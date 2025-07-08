42cb75cd75aac03ccd77fdfef605e834f3ba07f8
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-07-08 17:05:33 +0530

    Initial setup: Spring Boot + MySQL + Ticket model

---
+++ src/main/java/com/deepcoder/supportflow/SupportflowApplication.java
@@ -0,0 +1,13 @@
+package com.deepcoder.supportflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deepcoder/supportflow/SupportflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deepcoder.supportflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
